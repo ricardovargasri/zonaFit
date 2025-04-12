211700592892d8b84c0f21ec9154a46df6b5ee28
ricardo vargas <104957708+ricardovargasri@users.noreply.github.com>
2025-04-11 20:48:45 -0500

    se actualiza estado

--- src/main/java/com/ecodeuo/jdbc/datos/ClienteDao.java
+++ src/main/java/com/ecodeuo/jdbc/datos/ClienteDao.java
@@ -9,13 +9,18 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.ecodeuo.jdbc.conexion.Conexion.getConnection;
+
 public class ClienteDao implements IclienteDao{
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    Connection con = getConnection();
     @Override
     public List<Cliente> listarClientes() {
         List<Cliente> clientes = new ArrayList<>();// se cra lista de clientes
         PreparedStatement ps;
         ResultSet rs;
-        Connection con = Conexion.getConnection();
+        Connection con = getConnection();
         var sql = "SELECT * FROM clientes ORDER BY id";
 
         try {
@@ -45,11 +50,66 @@ public class ClienteDao implements IclienteDao{
 
     @Override
     public boolean buscarPorId(Cliente cliente) {
-        return false;
+
+        String sql = "SELECT * FROM clientes WHERE id = ?";
+
+        Connection con = getConnection();
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cliente.getId());
+            rs = ps.executeQuery();
+            if (rs.next()){
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setMembresia(rs.getInt("membresia"));
+                return true;
+            }
+        }catch (Exception e){
+            System.out.println("error de tipo "+e.getMessage());
+            return false;
+        }
+        finally {
+            try {
+                if (rs != null){
+                    rs.close();
+                }
+                if (ps != null){
+                    ps.close();
+                }
+                con.close();
+            }catch (Exception e){
+                System.out.println("no se pudo cerrar la conexion: "+e.getMessage());
+            }
+        }
+return false;
     }
 
     @Override
     public boolean agregarCliente(Cliente cliente) {
+
+        String sql = "INSERT INTO clientes (nombre, apellido, membresia) VALUES (?, ?, ?)";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, rs.getString("nombre"));
+            ps.setString(2, rs.getString("apellido"));
+            ps.setInt(3, rs.getInt("membresia"));
+            rs = ps.executeQuery();
+            int filasAfectadas = ps.executeUpdate();
+
+            return  filasAfectadas > 0;
+
+        }catch (Exception e){
+            System.out.println("error de tipo: " + e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            }catch (Exception e){
+                System.out.println("error de tipo: "+ e.getMessage());
+            }
+        }
+
         return false;
     }
 
@@ -65,7 +125,14 @@ public class ClienteDao implements IclienteDao{
 
     public static void main(String[] args) {
         var clienteDao = new ClienteDao();
-        var listado = clienteDao.listarClientes();
-        listado.forEach(System.out::println);
+        /*var listado = clienteDao.listarClientes();
+        listado.forEach(System.out::println);*/
+
+        var clientaso =  new Cliente(3);
+        var encontrado = clienteDao.buscarPorId(clientaso);
+        if(encontrado){
+            System.out.println("se encontro el cliente: " + clientaso);
+        }else
+            System.out.println("paila ñero");
     }
 }
